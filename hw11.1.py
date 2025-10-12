68eaba8dcaa62470e9048610399be61322f0cb6e
Sergo0196 <seregpets.ru@mail.ru>
2025-10-12 23:38:12 +0300

    delete

--- hw11.1.py
+++ hw11.1.py
@@ -1,12 +1,16 @@
-def fact(a):
-    ls = []
-    if a == 1:
-        return print('!1 = 1')
+def fact(a=0):
+    a = int(input("Введите натуральное число: "))
+    if a > 0: 
+        ls = []
+        if a == 1:
+            return print('!1 = 1')
+        else:
+            result = 1
+            for i in range(1, a + 1):
+                result *= i
+                ls.append(result)
+                sorted_ls = sorted(ls, reverse=True)
+            return print(sorted_ls)
     else:
-        result = 1
-        for i in range(1, a + 1):
-            result *= i
-            ls.append(result)
-            sorted_ls = sorted(ls, reverse=True)
-        return print(sorted_ls)
-fact(1)
+        print('Введенное число не является натуральным!')
+fact()
